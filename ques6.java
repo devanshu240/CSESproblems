40024a15742fe833a375f4a5f862e3ca34a8215f
devanshu240 <devanshukaushal46@gmail.com>
2024-05-24 23:16:08 +0530

    Initial commit

---
+++ ques6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ques6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- > 0){
+            int y = sc.nextInt();
+            int x = sc.nextInt();
+            int l = Math.max(y,x);
+            if(l % 2 == 0){
+                if(x == l){
+                    int r = (l-1)*(l-1)+y;
+                    System.out.println(r);
+                } else {
+                    int r = (l-1)*(l-1)+l+(l-x);
+                    System.out.println(r);
+                }
+            } else {
+                if(x == l){
+                    int r = (l-1)*(l-1)+x;
+                    System.out.println(r);
+                } else {
+                    int r = (l-1)*(l-1)+l+(l-y);
+                    System.out.println(r);
+                }
+            }
+        }
+
+    }
+}
